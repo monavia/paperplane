bba0f52cf6d27c94290ffaf18f306574760c2142
monavia <monavia95@gmail.com>
2026-07-25 22:40:51 +0700

    remove test files + CI pipeline

--- src/test/harness.ts
+++
@@ -1,7 +0,0 @@
-import request from "supertest";
-import { createApp } from "../bot/api/apiServer.js";
-
-export async function createTestServer() {
-  const app = await createApp();
-  return { app, request: request(app) };
-}
